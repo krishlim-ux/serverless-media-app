9b49772ee7825565f8a50d618e23226e6e6e6ea7
KL <unknownkiller@Unknowns-MacBook-Pro.local>
2026-06-29 15:56:12 +0100

    feat: setup private s3 buckets, cloudfront oac, and deployment pipeline

--- serverless_media_app/serverless_media_app_stack.py
+++ serverless_media_app/serverless_media_app_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    RemovalPolicy,
+    aws_s3 as s3,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as origins
 )
 from constructs import Construct
 
@@ -10,10 +12,34 @@ class ServerlessMediaAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # 1. Provision the private storage buckets
+        self.frontend_bucket = s3.Bucket(
+            self, "StaticWebsiteBucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ServerlessMediaAppQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        self.jpg_bucket = s3.Bucket(
+            self, "JpgMediaBucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+        )
+
+        self.pdf_bucket = s3.Bucket(
+            self, "PdfMediaBucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+        )
+
+        # 2. Deploy CloudFront pointing to the frontend bucket with Origin Access Control (OAC)
+        self.distribution = cloudfront.Distribution(
+            self, "MediaAppDistribution",
+            default_root_object="index.html",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3BucketOrigin.with_origin_access_control(self.frontend_bucket),
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+            )
+        )
